e4f06be19dcbd2f31744e118a7f71bddc29e61be
trungdeveloper <tantrung12011999@gmail.com>
2019-06-14 18:34:17 +0700

    bla

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,46 @@
 $(function() {
-  let a = $(".js-header-logo").height();
-  let previousScroll = 0;
-  $(window).scroll(function() {
-    var currentScroll = $(this).scrollTop();
-    if (currentScroll > previousScroll) {
-      if ($(".js-header-logo").height() > 30) {
-        $(".js-header-logo").css("height", a - currentScroll / 10);
+  if ($(document).width() > 1199) {
+    let a = $(".js-header-logo").height();
+    let previousScroll = 0;
+    $(window).scroll(function() {
+      var currentScroll = $(this).scrollTop();
+      if (currentScroll > previousScroll) {
+        if ($(".js-header-logo").height() > 30) {
+          $(".js-header-logo").css("height", a - currentScroll / 10);
+        }
+        if ($(".js-header-logo").height() < 30) {
+          $(".js-header-logo").css("height", 30);
+        }
+      } else {
+        if ($(".js-header-logo").height() < 86 && currentScroll < 550) {
+          $(".js-header-logo").css("height", 30 + (550 - currentScroll) / 10);
+        }
+        if ($(".js-header-logo").height() > 85) {
+          $(".js-header-logo").css("height", 85);
+        }
       }
-      if ($(".js-header-logo").height() < 30) {
-        $(".js-header-logo").css("height", 30);
-      }
-    } else {
-      if ($(".js-header-logo").height() < 86 && currentScroll < 550) {
-        $(".js-header-logo").css("height", 30 + (550 - currentScroll) / 10);
-      }
-      if ($(".js-header-logo").height() > 85) {
-        $(".js-header-logo").css("height", 85);
-      }
-    }
-    previousScroll = currentScroll;
-  });
-
-  $("#click").click(function() {
+      previousScroll = currentScroll;
+    });
+  }
+  $(".click").click(function() {
     $("#mySidenav").css("width", "340px");
-    // $("#main").css("margin-left", "340px");
     $(".navbar").css("margin-left", "340px");
-    $("section").css("margin-left", "340px");
+    $("#header").addClass("shift-img");
   });
   $(".closebtn").click(function() {
     $("#mySidenav").css("width", "0");
     $(".navbar").css("margin-left", "0");
-    // $("#main").css("margin-left", "0");
-    $("section").css("margin-left", "0");
+    $("#header").removeClass("shift-img");
+  });
+
+  $(document).click(function(e) {
+    if (
+      !$("#mySidenav, .click").is(e.target) &&
+      $("#mySidenav, .click").has(e.target).length === 0
+    ) {
+      $("#mySidenav").css("width", "0");
+      $(".navbar").css("margin-left", "0");
+      $("#header").removeClass("shift-img");
+    }
   });
 });
